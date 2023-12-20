d711ccded9a731d58de5ca6cf12693348205ca7b
David Rivero <davidmiguel22573@gmail.com>
2023-12-19 17:21:05 -0800

    Extraction methods added

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -1,30 +1,16 @@
+from solution import Solution
+
+
 def read_input(path: str) -> list:
     with open(path, "r") as file:
         return [line.strip() for line in file.readlines()]
-
-
-class Solution:
-    def __init__(self, content) -> None:
-        self.content = content
-        self.categories = []
-        self.parse_content()
-
-    def parse_content(self):
-        content = self.content
-        self.content = content
-
-    def part_one(self):
-        pass
-
-    def part_two(self):
-        pass
 
 
 def main():
     paths = ['test.txt', 'input.txt']
     for path in paths:
         content = read_input(path)
-        solution = None
+        solution = Solution(content).solve()
         print(f"The lowest location number in {path}: {solution}")
 
 
