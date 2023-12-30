c9d872a497f32a6b2fb151c32170cb3c6b9ac803
david22573 <davidmiguel22573@gmail.com>
2023-12-29 20:42:52 -0800

    Day 5 - revert solutions

--- 2023/day5/solution.py
+++ 2023/day5/solution.py
@@ -1,69 +1,63 @@
-def extract_nums(line: str) -> tuple[tuple[int, int], int]:
-    """Extracts source ranges and destination from a line of input."""
+def extract_nums(line: str):
     nums = [int(num) for num in line.split()]
     destination, source, length = nums
     source_ranges = (source, source + length - 1)
-    return source_ranges, destination
+    return (source_ranges, destination)
 
 
-def map_seed(seed: int, ranges: list) -> int:
-    """Maps a seed to its new location based on given ranges."""
-    for source_ranges, destination in ranges:
-        if source_ranges[0] <= seed <= source_ranges[1]:
-            offset = seed - source_ranges[0]
+def seed_in_range(seed: int, source_ranges: tuple):
+    start_range, stop_range = source_ranges
+    return start_range <= seed <= stop_range
+
+
+def map_seed(seed: int, ranges: list):
+    for r in ranges:
+        source_ranges, destination = r
+        if seed_in_range(seed, source_ranges):
+            start_range, stop_range = source_ranges
+            offset = seed - start_range
             return destination + offset
-    return seed  # No matching range, return the original seed
+    return seed
 
 
-def extract_seeds(line: str) -> list[tuple[int, int]]:
-    """Extracts seed pairs (starting seed, range) from the first line."""
+def extract_seeds(line: str):
     line = line.split(":")[1].strip()
-    seeds = line.split()
-    return [(int(seeds[i]), int(seeds[i + 1])) for i in range(0, len(seeds), 2)]
+    return [int(num) for num in line.split()]
+
 
 
 class Solution:
-    def __init__(self, content: list[str]) -> None:
+    def __init__(self, content: list[str]):
         self.content = content
 
-    def part_one(self) -> int:
-        """Calculates the minimum seed location for part one."""
-        seeds = extract_seeds(self.content[0])
+    def get_min_location(self, seeds):
         seed_locations = []
         ranges = []
+        for seed in seeds:
+            current_seed = seed
+            for line in self.content[2:]:
+                if line == "" or line == self.content[-1]:
+                    next_seed = map_seed(current_seed, ranges)
+                    ranges = []
+                    current_seed = next_seed
+                elif line[0].isdigit():
+                    nums = extract_nums(line)
+                    ranges.append(nums)
+            seed_locations.append(current_seed)
+        return seed_locations
 
-        for starting_seed, range_ in seeds:
-            for seed in range(starting_seed, starting_seed + range_):
-                for line in self.content[2:]:
-                    if line == "" or line == self.content[-1]:
-                        seed = map_seed(seed, ranges)
-                        ranges = []
-                    elif line[0].isdigit():
-                        source_ranges, destination = extract_nums(line)
-                        ranges.append((source_ranges, destination))
-                seed_locations.append(seed)
 
-        return min(seed_locations)
 
-    def part_two(self) -> int:
-        """Calculates the minimum seed location for part two."""
+    def part_one(self):
         seeds = extract_seeds(self.content[0])
-        seed_locations = []
-
-        for starting_seed, range_ in seeds:
-            for seed in range(starting_seed, starting_seed + range_):
-                for line in self.content[2:]:
-                    if line == "" or line == self.content[-1]:
-                        seed = map_seed(seed, ranges)
-                        ranges = []
-                    elif line[0].isdigit():
-                        source_ranges, destination = extract_nums(line)
-                        ranges.append((source_ranges, destination))
-                seed_locations.append(seed)
-
+        seed_locations = self.get_min_location(seeds)
         return min(seed_locations)
 
-    def solve(self, part_two=False) -> int:
-        """Solves the problem for the specified part."""
-        return self.part_two() if part_two else self.part_one()
+    def part_two(self):
+         
 
+    def solve(self, part_two=False):
+        if part_two:
+            return self.part_two()
+        else:
+            return self.part_one()
